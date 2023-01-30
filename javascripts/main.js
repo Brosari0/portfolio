74e9b4f0a3a6e17544b58d442dfde677140fcd07
brosari0 <Trumpetskickbrass86@gmail.com>
2023-01-30 21:15:12 +0000

    remove javascript

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,8 @@
-const titleEl = document.querySelector("#title");
+// const titleEl = document.querySelector("#title");
 
-titleEl.addEventListener("mousemove", (e) => {
-  titleEl.style.backgroundPositionX = -e.offsetX / 10 + "px";
-  titleEl.style.backgroundPositionY = -e.offsetY / 10 + "px";
+// titleEl.addEventListener("mousemove", (e) => {
+//   titleEl.style.backgroundPositionX = -e.offsetX / 10 + "px";
+//   titleEl.style.backgroundPositionY = -e.offsetY / 10 + "px";
 
-});
+// });
 
