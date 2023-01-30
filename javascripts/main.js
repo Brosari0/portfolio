d7ba597ebdf59da83c9c26db5a56e2200f8c9870
brosari0 <Trumpetskickbrass86@gmail.com>
2023-01-30 20:01:27 +0000

    fix skills page, and resize carousel

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,8 @@
-// const titleEl = document.querySelector("#title");
+const titleEl = document.querySelector("#title");
 
-// titleEl.addEventListener("mousemove", (e) => {
-//   titleEl.style.backgroundPositionX = -e.offsetX + "px";
-//   titleEl.style.backgroundPositionY = -e.offsetY + "px";
-// });
+titleEl.addEventListener("mousemove", (e) => {
+  titleEl.style.backgroundPositionX = -e.offsetX / 10 + "px";
+  titleEl.style.backgroundPositionY = -e.offsetY / 10 + "px";
+
+});
+
